782af663cffa30209698cd01f1d7ddd02314aac7
雨食 <2500728177@qq.com>
2023-12-25 16:17:08 +0800

    dao层编写

---
+++ src/main/java/com/example/dao/UserDao.java
@@ -0,0 +1,15 @@
+package com.example.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Auther: 2500728177@qq.com
+ * @Date: 2023/12/25
+ * @Description: com.example.dao
+ * @Version: 1.0
+ */
+@Mapper
+public interface UserDao extends BaseMapper<User> {
+}
